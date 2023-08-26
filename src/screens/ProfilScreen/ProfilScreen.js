ce858868ffcd253bfcc0b83794f59e1b54969f6d
dziri ahmed <dziriahmed@issatso.u-sousse.tn>
2023-08-26 02:30:51 +0200

    auth with redux

--- src/screens/ProfilScreen/ProfilScreen.js
+++ src/screens/ProfilScreen/ProfilScreen.js
@@ -1,18 +1,53 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity,Image } from "react-native";
 
 import { getAuth } from "firebase/auth";
 import styles from "./profilScreenStyles";
+import { useSelector} from "react-redux";
 
 const ProfilScreen = () => {
   const onSignoutPress = () => {
     getAuth().signOut().then(console.log("signed out"));
   };
+  const user = useSelector((state)=>state.user)
   return (
     <View style={styles.container} >
       <Text>Welcome to profil !!</Text>
+{/* the user photo name and email */}
+      <View style={styles.firstsection} >
+        <Image  source={require('../../../assets/user.png')}/>
+        <View>
+          <Text>{user.name}</Text>
+          <Text>{user.email}</Text>
+        </View>
+      </View>
+
+ {/* User action */}
+
+      {/* Edit user info */}
+      <TouchableOpacity>
+        <Text>
+          Edit profil
+        </Text>
+      </TouchableOpacity>
+
+      {/* See user History */}
+      <TouchableOpacity>
+        <Text>
+          History
+        </Text>
+      </TouchableOpacity>
+
+      {/* Edit user info */}
+      <TouchableOpacity>
+        <Text>
+          Help
+        </Text>
+      </TouchableOpacity>
+
+      {/* Log out end user session */}
       <TouchableOpacity onPress={onSignoutPress}>
-        <Text>Sign Out</Text>
+        <Text>Log Out</Text>
       </TouchableOpacity>
     </View>
   );
--- src/screens/ProfilScreen/profilScreenStyles.js
+++ src/screens/ProfilScreen/profilScreenStyles.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { StyleSheet } from "react-native";
+import { Directions } from "react-native-gesture-handler";
 
 const styles=StyleSheet.create({
     container:{
-        justifyContent:'center',
-        alignItems:'center'
+        flex:1,
+       paddingHorizontal:30,
+       backgroundColor:'#fff'
+    },
+    firstsection:{
+        flexDirection:'row',
+        alignItems:'center',
+        marginVertical:30,
+        paddingVertical:20,
+        borderBottomWidth:3,
+        borderColor:'#DADADA',
+        
+        
     }
 })
 export default styles
